b8a5a489ffa77a1b3f7e9af5ba25d3df50cdb879
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-03-25 19:21:19 +0300

    add endpoints in app.py for adding a book and retrieving all books

--- api/app/app.py
+++ api/app/app.py
@@ -1,47 +1,65 @@
 #app.py
-'''import Flask module from flask package.
-Flask creates instances of web application'''
-
-
-from flask import request, jsonify, abort
-#request for handling request
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 from models import Books, Users
 import json
 
-    
-app = Flask(__name__, instance_relative_config=True)
+def create_app():   
+    app = Flask(__name__, instance_relative_config=True)
+    app.config["TESTING"] = True
 
-book = Books()
+    book = Books()
 
-@app.route('/books', methods=['POST', 'GET'])
-def put_and_get_books():
-#add a book method="POST"
-    if request.method == 'POST':
-        title = request.data.get('title')
-        author = request.data.get('author')
-        response = book.put(title, author)				
-        response.status_code = 200
+    @app.route('/books', methods=['POST', 'GET'])
+    def put_and_get_books():
+        #add a book method="POST"
+        if request.method == 'POST':
+            title = str(request.form.get('title'))
+            author = str(request.form.get('author'))
 
-        return response
+            #book = Books()
+            book.put(title, author)
+            response = jsonify({"title" : title, "author" : author})                
+            response.status_code = 200
+
+            return response
         
-#get a book method="GET"    
-    else:
-        all_books = book.get_all()
+        #get a book method="GET"    
+        else:
+            all_books = book.get_all()
 
-        results = []
-        for i in all_books:
-            obj = {
-                'title' : i,
-                'author' : all_books[i]
-				}
-            #append obj to results
-            results.append(obj)
-        response = jsonify(results)
-        response.status_code = 200
+            results = []
+            for i in all_books:
+                obj = {
+                    'title' : i,
+                   'author' : all_books[i]
+                    }
+                #append obj to results
+                results.append(obj)
+            response = jsonify(results)
+            response.status_code = 200
 
-        return response
+            return response
 
-    
+    return app
+'''
+    @app.route('/books/<book_id>', methods=['PUT', 'GET', 'DELETE'])
+    def modify_books():
+        if request.method == 'PUT':
+
+        else if request.method =='GET':
+
+        else:
+
+    @app.route('/users/books/<book_id>')
+    def borrow_book():
+
+
+    @app.route('/auth/register')
+  
+    @app.route('/auth/login')
+
+    @app.route('/auth/logout')
+
+    @app.route('/auth/reset-password')  '''
 
 
