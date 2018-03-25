874f91dda5e25478896b242d1bc1004a57bab2cc
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-03-25 19:16:28 +0300

    add test for add book and get all books endpoints

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,38 +1,37 @@
-import os
-import unittest
-import json
-import app, flask
+#test_api.py
+import unittest, json
 
+from api import app 
 
 #class to respresent books testcase
 class TestCreateBooks(unittest.TestCase):
     def setUp(self):
         #create a test client
-        
-        self.app = app.app.test_client()
-        self.user = Users()
-         
-             
+        self.app = app.app.create_app()
+        self.client = self.app.test_client  
+
         self.book= {"title":"Figure skating for dummies", "author":"Jonas Jonas"}
     
     #test that API can create a book (POST request)
     def test_api_can_create_books(self):
-        response = self.app.post('/books', data=self.book)
+        response = self.client().post('/books', data=self.book)
         self.assertEqual(response.status_code, 200)
-        self.assertIn({"title":"Figure skating for dummies"}, response.data)
-
-class TestGetAllBooks(unittest.TestCase):
-    def setUp(self):
-        self.app = app.app.test_client()
-
+        self.assertIn("Figure skating for dummies", response.data)
+    
     #test that api can get all books
     def test_api_can_get_all_books(self):
-        response = self.app.post('/books', data=self.book)
+        response = self.client().post('/books', data=self.book)
         self.assertEqual(response.status_code, 200)
-        res = self.app.get('/books')
-        self.assertEqual(res.status_code, 200)
-        self.assertIn({"title":"Figure skating for dummies"}, str(res.data))
+        response = self.client().get('/books')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Figure skating for dummies", str(response.data))
 
+    #test that api can delete book
+    #test that api can modify book
+    #test that api can retrieve book by id
+
+
+   
 
 if __name__ == "__main__":
     unittest.main()
