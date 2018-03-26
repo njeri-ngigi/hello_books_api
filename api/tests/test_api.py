3bc027859452afb837f9f812a29869dd8e4ffa46
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-03-26 12:15:55 +0300

    add unittests in test_api.py for get_book_by_id, edit_book and delete_book

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,16 +1,18 @@
+#my_api_test.py
+
 #test_api.py
 import unittest, json
 
 from api import app 
 
-#class to respresent books testcase
-class TestCreateBooks(unittest.TestCase):
+#class to respresent create an get books testcase
+class TestCreateAndGetAllBooks(unittest.TestCase):
     def setUp(self):
         #create a test client
         self.app = app.app.create_app()
         self.client = self.app.test_client  
 
-        self.book= {"title":"Figure skating for dummies", "author":"Jonas Jonas"}
+        self.book= {"book_id":1, "title":"Figure skating for dummies", "author":"Jonas Jonas"}
     
     #test that API can create a book (POST request)
     def test_api_can_create_books(self):
@@ -22,13 +24,53 @@ class TestCreateBooks(unittest.TestCase):
     def test_api_can_get_all_books(self):
         response = self.client().post('/books', data=self.book)
         self.assertEqual(response.status_code, 200)
+
         response = self.client().get('/books')
         self.assertEqual(response.status_code, 200)
         self.assertIn("Figure skating for dummies", str(response.data))
 
-    #test that api can delete book
+#class to represent test for modification of books by id
+class TestModifyBookById(unittest.TestCase):
+    def setUp(self):
+        #create a test client
+        self.app = app.app.create_app()
+        self.client = self.app.test_client
+        self.book= {"book_id":1, "title":"Figure skating for dummies", "author":"Jonas Jonas"}
+
+
+    #test that api can retrieve book by title
+    def test_api_can_get_book_by_id(self):
+        rv = self.client().post('/books', data=self.book)
+        self.assertEqual(rv.status_code, 200)
+        
+
+        #result_in_json = json.loads(rv.form.decode('utf-8').replace("'","\""))
+        result = self.client().get('/books/1')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn("Figure skating for dummies", str(result.data))
+
     #test that api can modify book
-    #test that api can retrieve book by id
+    def test_book_can_be_edited(self):
+        rv = self.client().post('/books', data={"book_id":2, "title":"The fault in our stars", "author":"Not John Green"})
+        self.assertEqual(rv.status_code, 200)
+
+        rv = self.client().put('/books/2', data={"book_id":2, "title":"The fault in our stars", "author":"John Greene"})
+        self.assertEqual(rv.status_code, 200)
+        results = self.client().get('/books/2')
+        self.assertIn("John Greene", str(results.data))
+
+    #test that api can delete book  
+    def test_delete_book(self):
+        rv = self.client().post('/books', data={"book_id":3, "title": "Queer cats, An African Tale", "author" : "Chimamano Nakote"})
+        self.assertEqual(rv.status_code, 200)
+
+        res = self.client().delete('/books/3')
+        self.assertEqual(res.status_code, 200)
+        #test to check whether deleted item exists
+        result = self.client().get('/books/3')
+        self.assertEqual(result.status_code, 404)
+
+    
 
 
    
