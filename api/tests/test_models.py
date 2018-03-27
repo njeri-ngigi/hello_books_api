9bf31f3254cace33490c557f4a4283d77c8e451a
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-03-27 05:56:42 +0300

    add test for edit or modify book

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -7,34 +7,32 @@ class TestModels(unittest.TestCase):
     def setUp(self):
         self.my_book = app.Books()
         self.my_user = app.Users()
-        self.my_book.books={1 : {"Hello world":"Leah Beau"}}
+        
         
     def test_put_book(self):
-        self.my_book.put("Harry Potter", "JK Rowling", 2)
-        self.assertIn("Harry Potter", self.my_book.books[2])
+        result = self.my_book.put("Harry Potter", "JK Rowling", "1st", 4, 2)
+        self.assertIn("Harry Potter", result['title'])
     
     def test_get_book(self):
-        result = self.my_book.get_all()
-        self.assertEqual(self.my_book.books, result)
+        result = self.my_book.put("Hello world", "Leah Beau", "3rd", 3, 1)
+        result = self.my_book.get_all()        
+        self.assertTrue(result)
 
     def test_edit_book(self):
-        self.my_book.put("Harry Potter", "JK Rowling", 2)
-        self.assertIn("Harry Potter", self.my_book.books[2])
-
-        result = self.my_book.edit_book("Harry Potter and the Goblet of Giggles", "JK Rowling", 2)
-        self.assertIn("Harry Potter and the Goblet of Giggles", self.my_book.books[2])
+        result = self.my_book.edit_book("Harry Potter and the Goblet of Giggles", "JK Rowling", "2nd", 2, 2)
+        self.assertIn("Harry Potter and the Goblet of Giggles", result["title"])
 
     def test_get_single_book(self):
-        self.my_book.put("Harry Potter", "JK Rowling", 2)
-        self.assertIn("Harry Potter", self.my_book.books[2])
         result = self.my_book.get_single_book(2)
-        self.assertEqual(result, {"Harry Potter" : "JK Rowling"})
+        self.assertEqual("JK Rowling", result['author'])
         
     def test_delete_book(self):
-        self.my_book.books[2]={"Harry Potter" : "JK Rowling"}
-        result = self.my_book.delete(2)
+        result = self.my_book.put("Play hard", "Eustace", "1st", 4, 3)
+        self.assertIn("Play hard", result['title'])
+
+        result = self.my_book.delete(3)
           
-        self.assertEqual(self.my_book.books, result)
+        self.assertNotIn(3, result)
 
     def test_put_user(self):
     	result = self.my_user.put("njeri-ngigi", "abc@a.com", "imeowit")
