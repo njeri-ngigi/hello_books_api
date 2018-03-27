4a0a7f5ffad8baf1808a98309081ba25460b4efc
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-03-27 05:57:42 +0300

    modify tests for api endpoints

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,7 +1,8 @@
 #my_api_test.py
 
 #test_api.py
-import unittest, json
+import unittest
+from flask import jsonify, json
 
 from api import app
 
@@ -11,17 +12,18 @@ class TestCreateAndGetAllBooks(unittest.TestCase):
         #create a test client
         self.client = app.app.test_client  
 
-        self.book= {"book_id":1, "title":"Figure skating for dummies", "author":"Jonas Jonas"}
+        self.testbook= {"book_id":1, "title":"Figure skating for dummies", "author":"Jonas Jonas", "edition":"3rd", "copies":20}
     
     #test that API can create a book (POST request)
     def test_api_can_create_books(self):
-        response = self.client().post('/books', data=self.book)
+        response = self.client().post('/books', data=self.testbook)
+
         self.assertEqual(response.status_code, 200)
         self.assertIn("Figure skating for dummies", response.data)
     
     #test that api can get all books
     def test_api_can_get_all_books(self):
-        response = self.client().post('/books', data=self.book)
+        response = self.client().post('/books', data=self.testbook)
         self.assertEqual(response.status_code, 200)
 
         response = self.client().get('/books')
@@ -34,26 +36,29 @@ class TestModifyBookById(unittest.TestCase):
         #create a test client
         
         self.client = app.app.test_client
-        self.book= {"book_id":1, "title":"Figure skating for dummies", "author":"Jonas Jonas"}
+        self.testbook= {"book_id":5, "title":"Figure skating for dummies", "author":"Jonas Jonas", "edition":"3rd", "copies":20}
 
 
-    #test that api can retrieve book by title
+    #test that api can retrieve book by id
     def test_api_can_get_book_by_id(self):
-        rv = self.client().post('/books', data=self.book)
+        rv = self.client().post('/books', data=self.testbook)
         self.assertEqual(rv.status_code, 200)
         
 
-        #result_in_json = json.loads(rv.form.decode('utf-8').replace("'","\""))
-        result = self.client().get('/books/1')
+       
+        result = self.client().get('/books/5')
+        
+        print (result)
         self.assertEqual(result.status_code, 200)
+        
         self.assertIn("Figure skating for dummies", str(result.data))
 
     #test that api can modify book
     def test_book_can_be_edited(self):
-        rv = self.client().post('/books', data={"book_id":2, "title":"The fault in our stars", "author":"Not John Green"})
+        rv = self.client().post('/books', data={"book_id":2, "title":"The fault in our stars", "author":"Not John Green", "edition":"7th", "copies":30})
         self.assertEqual(rv.status_code, 200)
 
-        rv = self.client().put('/books/2', data={"book_id":2, "title":"The fault in our stars", "author":"John Greene"})
+        rv = self.client().put('/books/2', data={"title":"The fault in our stars", "author":"John Greene", "eidition":"7th", "copies": 29})
         self.assertEqual(rv.status_code, 200)
         results = self.client().get('/books/2')
         self.assertIn("John Greene", str(results.data))
