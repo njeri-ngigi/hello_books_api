90c5d15b1e9acceeac46e47b2a8dd65ace94190d
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-04-01 17:44:19 +0300

    [bug] test heroku

---
+++ models.py
@@ -0,0 +1,115 @@
+'''models.py containing models for the API'''
+import random
+import string
+from werkzeug.security import generate_password_hash, check_password_hash
+
+ALL_BOOKS = {}
+
+class Books():
+    '''class to represent book model'''
+    def __init__(self):
+        self.book = {}
+
+    def get_all(self):
+        '''return all books from ALL_BOOKS dictionary'''
+        return ALL_BOOKS
+
+    def get_single_book(self, book_id):
+        '''get single book from ALL_BOOKS using id'''
+        if book_id in ALL_BOOKS:
+            return ALL_BOOKS[book_id]
+
+        return {"message":"Book not found"}
+
+    def put(self, title, author, edition, book_id):
+        '''add a book to ALL_BOOKS'''
+        if book_id in ALL_BOOKS:
+            return {"message":"Book id entered already exists"}
+
+        self.book["title"] = title
+        self.book["author"] = author
+        self.book["edition"] = edition
+
+        ALL_BOOKS[book_id] = self.book
+        return ALL_BOOKS[book_id]
+
+    #edit a book
+    def edit_book(self, title, author, edition, book_id):
+        '''edit a book by its id'''
+        if book_id in ALL_BOOKS:
+            self.book["title"] = title
+            self.book["author"] = author
+            self.book["edition"] = edition
+
+            ALL_BOOKS[book_id] = self.book
+            return ALL_BOOKS[book_id]
+        return {"message":"Book you are trying to edit doesn't exist"}
+
+    def delete(self, book_id):
+        '''delete a book by its id'''
+        if book_id in ALL_BOOKS:
+            del ALL_BOOKS[book_id]
+            return {"message":"Book {} deleted successfully".format(book_id)}
+
+        return {"message":"Book you are trying to delete doesn't exist"}
+
+B = Books()
+B2 = Books()
+B3 = Books()
+B4 = Books()
+B.put("Tiny Stone", "Martha Mackenzie", "1st", 1)
+B2.put("Fly away birdie", "Marietta Gonzalez", "1st", 2)
+B3.put("Go home, Susan", "Barry White", "1st", 3)
+B4.put("Dunia haina huruma", "Kung'u Kahiga", "1st", 4)
+
+
+USERS = {}
+
+class Users():
+    '''class to represent users model'''
+    def __init__(self):
+        self.user = {}
+
+    def put(self, name, username, email, password):
+        '''add a user to USERS'''
+        if username in USERS:
+            return {"message":"Username already exists"}
+        
+        self.user["name"] = name
+        self.user["email"] = email
+        pw_hash = generate_password_hash(password)
+        self.user["password"] = pw_hash
+
+        USERS[username] = self.user
+        return {username : USERS[username]}
+
+    def verify_password(self, username, password):
+        '''verify password'''
+        if username in USERS:
+            result = check_password_hash(USERS[username]["password"], password)
+            if result is True:
+                return "True"
+            return {"message": "Password incorrect"}
+        return {"message": "Incorrect username"}
+    
+    def borrow_book(self, book_id):
+        '''borrow a book by book_id'''
+        if book_id in ALL_BOOKS:
+            return {"message":"Book successfully checked out"}
+        return {"message":"Book not found"}
+    
+    def reset_password(self, username):
+        '''reset user password'''
+        if username in USERS:
+            new_password = ''.join(
+                random.choice(string.ascii_uppercase + string.digits) for _ in range(7))
+            pw_hash = generate_password_hash(new_password)
+            USERS[username]["password"] = pw_hash
+
+            return {"new password":new_password}
+
+        return {"message":"Incorrect username"}
+
+U = Users()
+U.put("Njeri Ngigi", "njeri-ngigi", "abc@d.com", "1234")
+U.put("Shaggy", "scooby", "scooby@you.com", "1234")
