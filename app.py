f1b85d130f6cc4d5dea9277a7f5ec9970fbf3d76
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-04-03 16:28:02 +0300

    [fix] update directory structure

--- app.py
+++ app.py
@@ -2,13 +2,31 @@
 import re
 from flask import Flask, request, jsonify
 from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_raw_jwt)
-from models import Books, Users
-
+import models
 
 app = Flask(__name__)
 app.config["TESTING"] = True
 
-MY_BOOK = Books()
+MY_BOOK = models.Books()
+
+@app.errorhandler(400)
+def bad_request(error):
+    '''error handler for Bad request'''
+    return jsonify(dict(error='Bad request')), 400
+@app.errorhandler(404)
+def page_not_found(error):
+    '''error handler for 404'''
+    return jsonify(dict(error='Page not found')), 404
+
+@app.errorhandler(405)
+def unauthorized(error):
+    '''error handler for 405'''
+    return jsonify(dict(error='Unauthorized method')), 405
+
+@app.errorhandler(500)
+def server_error(error):
+    '''error handler for 404'''
+    return jsonify(dict(error='Internal server error')), 500
 
 #sent_data = request.get_json(force=True)
 @app.route('/')
@@ -20,15 +38,22 @@ def books():
     if request.method == 'POST':
     #add a book method="POST"
         data = request.get_json()
+        if not data:
+            return jsonify({"message": "Fields cannot be empty"})
+            
         title = data.get('title')
         author = data.get('author')
         edition = data.get('edition')
         book_id = data.get('book_id')
-        response = jsonify(MY_BOOK.put(title, author, edition, book_id))
-        response.status_code = 200
+        status = data.get('status')
 
-        return response
+        if status == "available" or status == "unavailable":
+            response = jsonify(MY_BOOK.put(title, author, edition, book_id, status))
+            response.status_code = 200
 
+            return response
+
+        return jsonify({"message" : "Status has to be either available or unavailable"})
     #get a book method="GET"
     get_books = MY_BOOK.get_all()
     response = jsonify(get_books)
@@ -49,22 +74,33 @@ def book_book_id(book_id):
 
     elif request.method == 'PUT':
     #modify or edit a book method
+        edit_book = models.ALL_BOOKS[book_id]
         data = request.get_json()
+        if not data:
+            return jsonify({"message": "Fields cannot be empty"})
         title = data.get('title')
         author = data.get('author')
         edition = data.get('edition')
-        response = jsonify(MY_BOOK.edit_book(title, author, edition, book_id))
+        status = data.get('status')
+        if title is None:
+            title = edit_book["title"]
+        if author is None:
+            author = edit_book["author"]
+        if edition is None:
+            edition = edit_book["edition"]
+        if status is None:
+            status = edit_book["status"]
+
+        response = jsonify(MY_BOOK.edit_book(title, author, edition, book_id, status))
         response.status_code = 200
 
         return response
-
-    
+  
     #delete a book, method=DELETE
     response = jsonify(MY_BOOK.delete(book_id))
     response.status_code = 200
 
     return response
-
 
 #check if jwt token is in blacklist
 app.config['JWT_SECRET_KEY'] = 'my-key'
@@ -74,19 +110,20 @@ jwt = JWTManager(app)
 
 BLACKLIST = set()
 
-
 @jwt.token_in_blacklist_loader
 def check_if_token_blacklist(decrypted_token):
     '''check if jti(unique identifier) is in black list'''
     jti = decrypted_token['jti']
     return jti in BLACKLIST
 
-MY_USER = Users()
+MY_USER = models.Users()
 
 @app.route('/api/v1/auth/register', methods=['POST'])
 def register():
     '''endpoint to register a user'''
     data = request.get_json()
+    if not data:
+        return jsonify({"message": "Fields cannot be empty"})
     username = data.get('username')
     name = data.get('name')
     email = data.get('email')
@@ -102,9 +139,8 @@ def register():
     if match is None:
         return jsonify({"message": "Enter a valid email address"})
     response = jsonify(MY_USER.put(name, username, email, password))
-    response.status_code = 200
+    response.status_code = 201
     return response
-
 
 @app.route('/api/v1/users/books/<int:book_id>', methods=['POST'])
 @jwt_required
@@ -114,23 +150,23 @@ def users_books(book_id):
     response.status_code = 200
     return response
 
-
 @app.route('/api/v1/auth/login', methods=['POST'])
 def login():
     '''login user by verifying password and creating an access token'''
     data = request.get_json()
+    if not data:
+        return jsonify({"message": "Fields cannot be empty"})
     username = data.get('username')
     password = data.get('password')
     auth = MY_USER.verify_password(username, password)
 
     if auth == "True":
         access_token = create_access_token(identity=username)
-        return access_token
+        return jsonify(dict(token = access_token, message = "Login successful"))
 
     response = jsonify(auth)
     response.status_code = 401
     return response
-
 
 @app.route('/api/v1/auth/logout', methods=['POST'])
 @jwt_required
@@ -138,19 +174,19 @@ def logout():
     '''logout user by revoking password'''
     jti = get_raw_jwt()['jti']
     BLACKLIST.add(jti)
-    return jsonify({"message": "Successfully logged out"})
-
+    return jsonify({"message": "Successfully logged out"}), 200
 
 @app.route('/api/v1/auth/reset-password', methods=['POST'])
 def reset_password():
     '''reset user password'''
     data = request.get_json()
+    if not data:
+        return jsonify({"message": "Fields cannot be empty"})
     username = data.get("username")
 
     response = jsonify(MY_USER.reset_password(username))
     response.status_code = 200
     return response
-
 
 #method to run app.py
 if __name__ == '__main__':
--- tests/test_models.py
+++ tests/test_models.py
@@ -12,8 +12,8 @@ class TestModels(unittest.TestCase):
 
     def test_put_book(self):
         '''test that put_book adds book to dictionary ALL_BOOKS'''
-        result = self.my_book.put("Harry Potter", "JK Rowling", "1st", 22)
-        self.assertIn("Harry Potter", result['title'])
+        result = self.my_book.put("Harry Potter", "JK Rowling", "1st", 22, "available")
+        self.assertEqual("Book added successfully", result["message"])
 
     def test_get_all_books(self):
         '''tests get_all retrieves all books in the dictionary'''
@@ -22,20 +22,21 @@ class TestModels(unittest.TestCase):
 
     def test_edit_book(self):
         '''test that edit_book edits'''
-        self.my_book.put("Make me laugh", "Louis Lenard", "1st", 20)
+        self.my_book.put("Make me laugh", "Louis Lenard", "1st", 20, "available")
         result = self.my_book.edit_book("Harry Potter and the Goblet of Giggles",
-                                        "Louis Lenard", "2nd", 20)
-        self.assertIn("Harry Potter and the Goblet of Giggles", result["title"])
+                                        "Louis Lenard", "2nd", 20, "available")
+        self.assertIn("Details edited successfully", result["message"])
 
     def test_get_single_book(self):
         '''test that get_single_book returns a book from dictionary ALL_BOOKS'''
-        self.my_book.put("How long to live", "Mary Howard", "1st", 19)
+        self.my_book.put("How long to live", "Mary Howard",
+                         "1st", 19, "available")
         result = self.my_book.get_single_book(19)
         self.assertEqual("Mary Howard", result['author'])
 
     def test_delete_book(self):
         '''test that delete_book deletes book from ALL_BOOKS'''
-        self.my_book.put("Leople", "Master Xi", "1st", 15)
+        self.my_book.put("Leople", "Master Xi", "1st", 15, "available")
         result = self.my_book.delete(15)
         self.assertEqual(
             {"message": "Book 15 deleted successfully"}, result)
@@ -43,7 +44,7 @@ class TestModels(unittest.TestCase):
     def test_put_user(self):
         '''test that put_user adds user to dictionary USERS'''
         result = self.my_user.put("Adelle Owino", "a-owino", "ac@ab.com", "imeowit")
-        self.assertIn("a-owino", result)
+        self.assertIn("user registered successfully", result["message"])
 
     def test_verify_password(self):
         '''test verify password'''
@@ -53,13 +54,15 @@ class TestModels(unittest.TestCase):
         '''test borrow a book'''
         result = self.my_user.borrow_book(1)
         self.assertEqual(result, {"message":"Book successfully checked out"})
+        result2 = self.my_user.borrow_book(1)
+        self.assertEqual(result2, {"message":"Book is currently unavailble"})
 
     def test_reset_password(self):
         '''test reset password'''
         self.my_user.put("Martha", "martha", "ac@ab.com", "kesho")
         result = self.my_user.reset_password("martha")
-        self.assertEqual(False, check_password_hash("kesho", result["new password"]))
-        self.assertEqual(7, len(result["new password"]))
+        self.assertEqual(False, check_password_hash("kesho", result["new_password"]))
+        self.assertEqual(7, len(result["new_password"]))
 
 
 #make the tests conviniently executable
